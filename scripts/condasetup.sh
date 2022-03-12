30d123a0981e06d9fca833a2ca52deffef4b4676
Eric Au <eric.au@berkeleylights.com>
2022-03-12 15:03:08 -0800

    backing up work settings from bll

--- scripts/condasetup.sh
+++ scripts/condasetup.sh
@@ -4,6 +4,7 @@ set -eou pipefail
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh \
 	&& bash Miniconda3-latest-MacOSX-x86_64.sh -b -y && rm Miniconda3-latest-MacOSX-x86_64.sh 
 
-#conda config --add channels defaults
-#conda config --add channels bioconda
-#conda config --add channels conda-forge
+conda config --add channels defaults
+conda config --add channels bioconda
+conda config --add channels conda-forge
+
