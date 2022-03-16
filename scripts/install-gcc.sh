5fcc5b3c9334e568986b4d2d66956b0d3be71491
Eric Au <eric.au@berkeleylights.com>
2022-03-15 23:24:20 -0700

    install gcc

---
+++ scripts/install-gcc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+MacOSX=10.1.0
+WORK_DIR=~/miniconda3/gcc-MacOSX
+
+mkdir -p $WORK_DIR && cd $WORK_DIR
+curl -L https://ftpmirror.gnu.org/gcc/gcc-${MacOSX}/gcc-${MacOSX}.tar.xz | tar xf -
+cd gcc-${MacOSX} && ./contrib/download_prerequisites
+mkdir build && cd build
+
+../configure --prefix=/usr/local/gcc-${MacOSX}-MacOSX --enable-checking=release --enable-languages=c,c++,fortran --disable-multilib --with-sysroot=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk --program-suffix=-${MacOSX}-MacOSX
+make -j $(nproc) && sudo make $(nproc) install-strip
+
+export PATH=/usr/local/gcc-${MacOSX}/bin:$PATH
+
+sudo rm -r $WORK_DIR
+
